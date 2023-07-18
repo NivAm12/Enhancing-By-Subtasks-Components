1ba866414c74e0faf1f0b2d69f4e71d2377f02c6
Niv Amos <49129250+NivAm12@users.noreply.github.com>
2023-07-18 16:06:03 +0300

    mps bug fix

--- utils/train.py
+++ utils/train.py
@@ -1,21 +1,24 @@
 import torch
 import torch.nn as nn
 from tqdm import tqdm
+import wandb
 
 
 def train(multi_head_model: nn.Module, heads_props: dict, train_args: dict):
-    # prepare the model weights:
+    # prepare the model weights for training:
     multi_head_model.train()
     for model_head in multi_head_model.heads.values():
         model_head.train()
 
     for epoch in tqdm(range(train_args["epochs"])):
-        # create the data loaders list, and iterate the batchs simultaneously
-        head_prop = [head_prop['train_loader'] for head_prop in heads_props.values()]
+        # create the data loaders list
+        train_loaders = [head_prop['train_loader'] for head_prop in heads_props.values()]
 
-        for i, combined_batch in enumerate(zip(*head_prop)):
+        # iterate the batches simultaneously
+        for i, combined_batch in tqdm(enumerate(zip(*train_loaders))):
             loss = None
 
             for task_batch, head_name in zip(combined_batch, heads_props.keys()):
-                output = multi_head_model(task_batch, head_name)
+                print(task_batch['input_ids'].shape, head_name)
+                # output = multi_head_model(task_batch, head_name)
                 
