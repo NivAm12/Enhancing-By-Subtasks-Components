478454edc2eeb3960f7f35525d82d14a31bbfdf6
Niv Amos <49129250+NivAm12@users.noreply.github.com>
2023-07-22 22:00:52 +0300

    add logger

--- train.py
+++ train.py
@@ -15,7 +15,7 @@ def train(multi_head_model: nn.Module, heads_props: dict, train_args: dict):
     for model_head in multi_head_model.heads.values():
         model_head.train()
 
-    optim  = train_args['optim']
+    optim = train_args["optim"](multi_head_model.parameters(), lr=0.001)
 
     for epoch in tqdm(range(train_args["epochs"])):
         # create the data loaders list
@@ -35,17 +35,28 @@ def train(multi_head_model: nn.Module, heads_props: dict, train_args: dict):
                 loss = critic(output.squeeze(), task_batch['labels'].float())
                 step_loss += loss * heads_props[head_name]['loss_weight']
 
+                wandb.log({f'{head_name}_loss': loss})
+
             step_loss.backward()
             optim.step()
+            wandb.log({'loss': step_loss})
 
 
 
 if __name__ == '__main__':
-    device = 'mps'
+
+    train_args = {
+        "epochs": 10,
+        "device": "cuda" if torch.cuda.is_available() else "cpu",
+        "lr": 0.001,
+        "optim": torch.optim.AdamW,
+        "batch_size": 32
+    }
+     
     model_name = 'microsoft/BiomedNLP-PubMedBERT-base-uncased-abstract-fulltext'
     config = AutoConfig.from_pretrained(model_name)
     tokenizer = AutoTokenizer.from_pretrained(model_name, model_max_length=config.max_position_embeddings)
-    pre_trained_model = AutoModel.from_pretrained(model_name).to(device)
+    pre_trained_model = AutoModel.from_pretrained(model_name).to(train_args["device"])
 
     torch.manual_seed(5)
     file_path = 'data/acronym_data.txt'
@@ -54,8 +65,8 @@ if __name__ == '__main__':
     dataset.preprocss_dataset()
 
     in_features = config.hidden_size
-    binari_head = ClassificationHead(in_features=in_features, out_features=1).to('mps')
-    four_labels_head = ClassificationHead(in_features=in_features, out_features=4).to('mps')
+    binari_head = ClassificationHead(in_features=in_features, out_features=1).to(train_args["device"])
+    four_labels_head = ClassificationHead(in_features=in_features, out_features=4).to(train_args["device"])
 
     classifiers = torch.nn.ModuleDict({
         "binari_head": binari_head,
@@ -64,16 +75,9 @@ if __name__ == '__main__':
 
     multi_head_model = MultiHeadModel(pre_trained_model, classifiers)
 
-    train_loader_for_acronym, _ = dataset.get_dataloaders(train_size=0.9, batch_size=8)
+    train_loader_for_acronym, _ = dataset.get_dataloaders(train_size=0.9, batch_size=train_args["batch_size"])
     # train_loader2, _ = dataset.get_dataloaders(train_size=0.9, batch_size=32)
 
-    optim = torch.optim.AdamW(multi_head_model.parameters(), lr=0.001)
-
-    train_args = {
-        "epochs": 2,
-        "device": "mps",
-        "optim": optim
-    }
 
     heads_props = {
         "binari_head": {
@@ -87,6 +91,11 @@ if __name__ == '__main__':
 
         # }
     }
+
+    run = wandb.init(
+        project="test_nlp",
+        config=train_args
+    )
 
     train(multi_head_model, heads_props, train_args)
 
