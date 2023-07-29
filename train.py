8c3d27a229649d386384dc906eec00caab05b617
Niv Amos <49129250+NivAm12@users.noreply.github.com>
2023-07-29 13:22:43 +0300

    modify the training for multi tasks with glue and other benchmarks

--- train.py
+++ train.py
@@ -3,15 +3,16 @@ import torch.nn as nn
 from tqdm import tqdm
 import wandb
 from transformers import AutoConfig, AutoTokenizer, AutoModel
-from data.acronymDataset import AcronymDataset
-from data.RelationExtraction.MedicalNERDataset import MedicalNERDataset
-from data.RelationExtraction.MedicalRCDataset import MedicalRCDataset
 from models.multiHeadModel import MultiHeadModel
-from models.heads import ClassificationHead, NERHead, RelationClassificationHead
+from models.heads import ClassificationHead
 import os
 import argparse
-from datasets import load_from_disk
 from torch.optim.lr_scheduler import CosineAnnealingLR
+from datasets import load_dataset, Dataset
+from transformers import DataCollatorForTokenClassification
+from torch.utils.data import DataLoader
+import numpy as np
+from evaluate import load
 
 
 def train(multi_head_model: nn.Module, heads_props: dict, train_args: argparse.Namespace):
@@ -38,12 +39,14 @@ def train(multi_head_model: nn.Module, heads_props: dict, train_args: argparse.N
         # calculate train and val losses per epoch
         train_epoch_loss, heads_train_losses = run_epoch(multi_head_model, train_loaders, heads_props, train_args,
                                                          optim, scheduler, do_train=True)
-        val_epoch_loss, heads_val_losses = run_epoch(multi_head_model, val_loaders, heads_props, train_args,
+        val_epoch_loss, heads_val_losses, head_evals_scores = run_epoch(multi_head_model, val_loaders, heads_props, train_args,
                                                      do_train=False)
+        
 
         wandb.log({'train_loss': train_epoch_loss, 'val_loss': val_epoch_loss})
         wandb.log({f'{key}_loss': val for key, val in heads_train_losses.items()})
         wandb.log({f'val_{key}_loss': val for key, val in heads_val_losses.items()})
+        wandb.log({f'{key}_score': val for key, val in head_evals_scores.items()})
 
         # save the model at each epoch
         if not os.path.exists(train_args.save_path):
@@ -59,6 +62,7 @@ def train(multi_head_model: nn.Module, heads_props: dict, train_args: argparse.N
 def run_epoch(model, data_loaders, heads_props, train_args, optim=None, scheduler=None, do_train=True):
     epoch_loss = 0.0
     head_losses = {head_name: 0.0 for head_name in heads_props.keys()}  # Dictionary to store the losses for each head
+    head_evals = {head_name: 0.0 for head_name in heads_props.keys()}  # Dictionary to store the losses for each head
 
     # prepare the model weights for training or validation:
     if do_train:
@@ -71,17 +75,22 @@ def run_epoch(model, data_loaders, heads_props, train_args, optim=None, schedule
         step_loss = 0.0
 
         for task_batch, head_name in zip(combined_batch, heads_props.keys()):
-            critic = heads_props[head_name]['loss_func']
+            head_props = heads_props[head_name]
+            critic = head_props['loss_func']
             task_batch = task_batch.to(train_args.device)
 
             with torch.set_grad_enabled(do_train):
                 output = model(task_batch, head_name)
-                loss = critic(output.squeeze(), task_batch['labels'].float()) if heads_props[head_name][
+                loss = critic(output.squeeze(), task_batch['labels'].float()) if head_props[
                     'loss_func'] else output
 
-                step_loss += loss * heads_props[head_name]['loss_weight']
+                step_loss += loss * head_props['loss_weight']
                 # Accumulate the head loss for the current batch
                 head_losses[head_name] += loss.item()
+                
+                # evaluate 
+                if not do_train:
+                    head_evals[head_name] += compute_metrics(output, task_batch['labels'], head_props['eval_metric'], head_props['eval_type'])
 
         epoch_loss += step_loss.item()
 
@@ -94,24 +103,83 @@ def run_epoch(model, data_loaders, heads_props, train_args, optim=None, schedule
     # Normalize head and total losses by the number of batches to get the average loss per epoch
     num_batches = len(data_loaders[0])
     head_losses = {head_name: loss / num_batches for head_name, loss in head_losses.items()}
+    head_evals = {head_name: loss / num_batches for head_name, loss in head_evals.items()}
     epoch_loss /= num_batches
 
-    return epoch_loss, head_losses
+    return epoch_loss, head_losses, head_evals
 
 
 def parse_args():
     parser = argparse.ArgumentParser(description="Script to train your model")
     parser.add_argument("--seed", type=int, default=0, help="Random seed")
     parser.add_argument("--epochs", type=int, default=10, help="Number of training epochs")
-    parser.add_argument("--device", type=str, default="cuda", choices=["cuda", "cpu", "mps"],
+    parser.add_argument("--device", type=str, default="mps", choices=["cuda", "cpu", "mps"],
                         help="Device to run training on")
     parser.add_argument("--lr", type=float, default=1e-5, help="Learning rate")
     parser.add_argument("--betas", nargs=2, type=float, default=[0.9, 0.999], help="Betas for AdamW optimizer")
     parser.add_argument("--weight_decay", type=float, default=0.01, help="Weight decay for optimizer")
     parser.add_argument("--batch_size", type=int, default=64, help="Batch size for training")
-    parser.add_argument("--save_path", type=str, default="models/weights", help="Path to save model weights")
+    parser.add_argument("--save_path", type=str, default="models/weights/multitask", help="Path to save model weights")
     parser.add_argument("--project", type=str, default="train_val_bertmed", help="Wandb project name to use for logs")
     return parser.parse_args()
+
+
+def load_data(dataset_benchmark: str, dataset_name: str, train_samples: int, test_samples: int):
+    train_dataset = load_dataset(dataset_benchmark, dataset_name, split=f"train[:{train_samples}]")
+    test_dataset = load_dataset(dataset_benchmark, dataset_name, split=f"validation[:{test_samples}]")
+
+    return {
+        "train": train_dataset,
+        "test": test_dataset
+    }
+
+
+def preprocess_func(examples, tokenizer):
+    return tokenizer(examples["sentence1"], examples["sentence2"], truncation=True)
+
+
+def preprocess_dataset(dataset: Dataset, tokenizer, preprocess_fn):
+    tokenized_datasets = dataset.map(preprocess_fn, fn_kwargs={"tokenizer": tokenizer})
+    tokenized_datasets = tokenized_datasets.rename_column("label", "labels")
+    tokenized_datasets.set_format("torch")
+
+    return tokenized_datasets
+
+def get_datloaders(dataset, tokenizer, val_size: float, batch_size: int, shuffle: bool):
+    split = dataset.train_test_split(test_size=val_size)
+    train_dataset = split["train"]
+    val_dataset = split["test"]
+
+    data_collator = DataCollatorForTokenClassification(tokenizer=tokenizer, return_tensors="pt", padding=True) 
+
+    train_dataloader = DataLoader(
+        train_dataset,
+        collate_fn=data_collator,
+        batch_size=batch_size,
+        shuffle=shuffle,
+        drop_last=True,
+        num_workers=0)
+
+    val_dataloader = DataLoader(
+        val_dataset,
+        collate_fn=data_collator,
+        batch_size=batch_size,
+        shuffle=shuffle,
+        drop_last=True,
+        num_workers=0)
+
+    return train_dataloader, val_dataloader
+
+def compute_metrics(logits, labels, metric, metric_type):
+
+    if metric_type == 'accuracy':
+        logits = nn.Sigmoid()(logits)
+        logits = np.round(logits).astype(np.int8)
+
+    result =  metric.compute(predictions=logits, references=labels)
+    result = result['accuracy'] if metric_type == 'accuracy' else result
+
+    return result
 
 
 if __name__ == '__main__':
@@ -120,71 +188,74 @@ if __name__ == '__main__':
     torch.cuda.empty_cache()
 
     # ----------------------------- Model ------------------------------------------------------------
-    model_name = 'microsoft/BiomedNLP-PubMedBERT-base-uncased-abstract-fulltext'
+    model_name = 'bert-base-uncased'
     config = AutoConfig.from_pretrained(model_name)
     tokenizer = AutoTokenizer.from_pretrained(model_name, model_max_length=config.max_position_embeddings)
     pre_trained_model = AutoModel.from_pretrained(model_name)
 
     # ----------------------------- Data ------------------------------------------------------------
     torch.manual_seed(train_args.seed)
-    # acronym
-    acronym_data_file_path = 'data/acronym_data.txt'
-    acronym_dataset = AcronymDataset(file_path=acronym_data_file_path, tokenizer=tokenizer)
-    acronym_train_loader, acronym_val_loader = acronym_dataset.get_dataloaders(train_size=0.9,
-                                                                               batch_size=train_args.batch_size)
-    # n2c2
-    n2c2_dataset_path = 'data/RelationExtraction/n2c2_dataset'
-    n2c2_dataset = load_from_disk(n2c2_dataset_path)
+    train_datasets_size = 2400
+    val_size = 0.1
 
-    # NER
-    medical_ner_dataset = MedicalNERDataset(n2c2_dataset, tokenizer, train_size=0.9, batch_size=8)
-    ner_data_loaders = medical_ner_dataset.get_dataloaders()
-    ner_train_loader = ner_data_loaders["train"]
-    ner_val_loader = ner_data_loaders["validation"]
+    # load data
+    task1_data = load_data('glue', 'mrpc', train_samples=train_datasets_size, test_samples=train_datasets_size)
+    task2_data = load_data('glue', 'rte', train_samples=train_datasets_size, test_samples=train_datasets_size)
+    task3_data = load_data('glue', 'stsb', train_samples=train_datasets_size, test_samples=train_datasets_size)
 
-    # RC
-    medical_rc_dataset = MedicalRCDataset(n2c2_dataset, tokenizer, pre_trained_model, train_size=0.9,
-                                          batch_size=train_args.batch_size)
-    rc_data_loaders = medical_rc_dataset.get_dataloaders()
-    rc_train_loader = rc_data_loaders["train"]
-    rc_val_loader = rc_data_loaders["validation"]
+    # preprocess
+    task1_train_dataset = preprocess_dataset(task1_data['train'], tokenizer, preprocess_func)
+    task2_train_dataset = preprocess_dataset(task2_data['train'], tokenizer, preprocess_func)
+    task3_train_dataset = preprocess_dataset(task3_data['train'], tokenizer, preprocess_func)
+
+    # data loaders
+    task1_train_dataloader, task1_val_dataloader = get_datloaders(task1_train_dataset, tokenizer, val_size=val_size, batch_size=train_args.batch_size, shuffle=True)
+    task2_train_dataloader, task2_val_dataloader = get_datloaders(task2_train_dataset, tokenizer, val_size=val_size, batch_size=train_args.batch_size, shuffle=True)
+    task3_train_dataloader, task3_val_dataloader = get_datloaders(task3_train_dataset, tokenizer, val_size=val_size, batch_size=train_args.batch_size, shuffle=True)
 
     # ----------------------------- Headers ------------------------------------------------------------
     in_features = config.hidden_size
-    acronym_head = ClassificationHead(in_features=in_features, out_features=1)
-    ner_head = NERHead(hidden_size=in_features, num_labels=len(medical_ner_dataset.id_2_label))
-    rc_head = RelationClassificationHead(hidden_size=in_features, num_labels=1)
+    task1_head = ClassificationHead(in_features=in_features, out_features=1)
+    task2_head = ClassificationHead(in_features=in_features, out_features=1)
+    task3_head = ClassificationHead(in_features=in_features, out_features=1)
+    
 
     classifiers = torch.nn.ModuleDict({
-        "acronym_head": acronym_head,
-        "ner_head": ner_head,
-        "rc_head": rc_head
+        "task1_head": task1_head,
+        "task2_head": task2_head,
+        "task3_head": task3_head
     })
 
     multi_head_model = MultiHeadModel(pre_trained_model, classifiers)
     multi_head_model.to(train_args.device)
 
     heads_props = {
-        "acronym_head": {
-            "train_loader": acronym_train_loader,
-            "val_loader": acronym_val_loader,
-            "loss_weight": 0.2,
-            "loss_func": torch.nn.BCEWithLogitsLoss(),
-            "require_batch_for_forward": False
+        "task1_head": {
+            "train_loader": task1_train_dataset,
+            "val_loader": task1_val_dataloader,
+            "loss_weight": 0.3,
+            "loss_func": nn.BCEWithLogitsLoss(),
+            "eval_metric": load('glue', 'mrpc'),
+            "eval_type": "accuracy",
+            "val_size": train_datasets_size * val_size
         },
-        "ner_head": {
-            "train_loader": ner_train_loader,
-            "val_loader": ner_val_loader,
-            "loss_weight": 0.4,
-            "loss_func": None,
-            "require_batch_for_forward": True
+        "task2_head": {
+            "train_loader": task2_train_dataset,
+            "val_loader": task2_val_dataloader,
+            "loss_weight": 0.3,
+            "loss_func": nn.BCEWithLogitsLoss(),
+            "eval_metric": load('glue', 'rte'),
+            "eval_type": "accuracy",
+            "val_size": train_datasets_size * val_size
         },
-        "rc_head": {
-            "train_loader": rc_train_loader,
-            "val_loader": rc_val_loader,
-            "loss_weight": 0.4,
+        "task3_head": {
+            "train_loader": task3_train_dataset,
+            "val_loader": task3_val_dataloader,
+            "loss_weight": 0.3,
             "loss_func": torch.nn.BCEWithLogitsLoss(),
-            "require_batch_for_forward": True
+            "eval_metric": nn.MSELoss(),
+            "eval_type": "regression",
+            "val_size": train_datasets_size * val_size
         }
     }
 
